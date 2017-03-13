e9330fb7031c028c0572ebb494ff6bb2d74e880d
kazuma-sato <kazuma.sato@georgebrown.ca>
2017-03-12 21:14:28 -0400

    entryPostHandler

--- api/Entries/entryPostHandler.js
+++ api/Entries/entryPostHandler.js
@@ -3,30 +3,24 @@
 	Lambda Function for entryPostHandler in crosscampus by xcampus API
 	by Kazuma Sato 100948212 kazuma.sato@georgebrown.ca
     Date created: Feb 2, 2017
-    Date last modified Mar 7, 2017
+    Date last modified Mar 12, 2017
 */
 
 console.log('\nLoading function entryPostHandler...\n');
 
-exports.entryPostHandler = function(event, context, callback) {
+exports.handler = function(event, context, callback) {
 
-	let entry = JSON.parse(event.key1);
+	let entry = event.entry;
 	const mysql = require('mysql');
-	const connection = mysql.createConnection(
-		
-		//require('../xcampusdb')
-
-		//For Testing :
-		{
-			host     : 'localhost',
-			user     : 'xcampus',
+	const connection = mysql.createConnection({
+			host     : 'db.crosscampus.xcamp.us',
+			user     : 'root',
 			password : 'GBCxcamp',
 			database : 'crosscampus',
 			port     : '3306',
-			//debug    : true
 		}
 	);
-	
+
 	connection.query(
 		'SELECT id FROM entryType WHERE name LIKE ?;',
 		entry.entryType,
@@ -37,11 +31,9 @@ exports.entryPostHandler = function(event, context, callback) {
 
 		if(error) {
 			connection.end();
-			context.fail();
 			callback(error);
 		} else if(!result.length) {
 			connection.end();
-			context.fail();
 			callback(new Error("Error: Entry type is invalid!!"));
 		} else {
 			console.log("Entry type ID is : " + entry.entryType);
@@ -51,14 +43,13 @@ exports.entryPostHandler = function(event, context, callback) {
 				entry,
 				selectAuthorCallback
 			);
-		};
+		}
 	}
 
 	function selectAuthorCallback(error, result) {
 
 		if(error) {
 			connection.end();
-			context.fail();
 			callback(error);
 		} else {
 			entry.id = result.insertId;
@@ -72,8 +63,7 @@ exports.entryPostHandler = function(event, context, callback) {
 				);
 			} else {
 				connection.end();
-				context.succeed();
-				callback(null, JSON.stringify(entry));
+				callback(null, entry);
 			}
 		}
 	}
@@ -82,11 +72,9 @@ exports.entryPostHandler = function(event, context, callback) {
 
 		if(error) {
 			connection.end();
-			context.fail();
 			callback(error);			
 		} else if(!result) {
 			connection.end();
-			context.fail();
 			callback(new Error("Error: ParentID, '" + entry.parentID + "', of entry " + entry.id + " is invalid!"));
 		} else {
 			entry.parentEntryAuthor = result[0].author;
@@ -102,7 +90,6 @@ exports.entryPostHandler = function(event, context, callback) {
 
 		if(error) {
 			connection.end();
-			context.fail();
 			callback(error);
 		} else {
 			console.log("Notification to user #" 
@@ -110,7 +97,7 @@ exports.entryPostHandler = function(event, context, callback) {
 						+ entry.id + " added.");
 			connection.end();
 			context.succeed();
-			callback(null, JSON.stringify(entry));
+			callback(null, entry);
 		}
 	}
-}
+};
